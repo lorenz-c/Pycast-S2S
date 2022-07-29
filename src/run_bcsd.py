82c58613db06c05e0d1917fe4415381c4598da58
Christof Lorenz <christof.lorenz@kit.edu>
2022-07-29 12:01:18 +0000

    Re-forged run_bcsd so that it calls all other subroutines without an additional layer

--- src/run_bcsd.py
+++ src/run_bcsd.py
@@ -4,35 +4,50 @@ import xarray as xr
 import dask
 import numpy as np
 from dask.distributed import Client, LocalCluster
-
-# import python-files
-from apply_bc import apply_bc
+import argparse
 
 import modules
-from io_module import slice_and_correct
-
-# f = open('src/bcsd_parameter.json')
-f = open('bcsd_parameter.json')
-parameter = json.load(f)
-
-# Set month and year of the current forecast
-month = parameter["issue_date"]["month"]
-year = parameter["issue_date"]["year"]
-
-####### Setup of Dask-Cluster required! ##########
-
-# Convert the domain names in the parameter JSON to an array:
-domain_names = [domain_names['name'] for domain_names in parameter["domains"]]
-
-i = 0
-#for i in range(0, 1): # domain_names
-# Run function
 
 
+def get_clas():
+    
+    parser = argparse.ArgumentParser(description="Python-based BCSD",
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+    parser.add_argument("-y", "--year", action="store_true", help="Year of the actual forecast")
+    parser.add_argument("-m", "--month", action="store_true", help="Month of the actual forecast")
+    parser.add_argument("-d", "--domain", action="store_true", help="Domain")
+    parser.add_argument("-p", "--parameter_file", help="Parameter file")
+    
+    
 
 if __name__ == '__main__':
+    
 
+    
+    
+    f = open('bcsd_parameter.json')
+    parameter = json.load(f)
+    
+    # Set month and year of the current forecast
+    month = parameter["issue_date"]["month"]
+    year = parameter["issue_date"]["year"]
+    
+    # Convert the domain names in the parameter JSON to an array:
+    domain_names = [domain_names['name'] for domain_names in parameter["domains"]]
+    
+    # Do only the Khuzestan-run
+    i = 1
+    
+    # Get some ressourcers
+    client, cluster = getCluster('rome', 1, 30)
+    
+    # Do the memory magic...
+    client.amm.start() 
+    
+    # Write some info about the cluster
+    print(client.scheduler_info)
+    
     # Set all the metadata for the output file
     global_attributes, variable_attributes = modules.set_metadata(15, 15)
 
@@ -44,39 +59,31 @@ if __name__ == '__main__':
 
     # Create an empty NetCDF in which we write the BCSD output
     ds = modules.create_4d_netcdf(bc_out_lns, global_attributes, variable_attributes, coords)
-
-    # Initialize a dictionary in order to store outputs of the BCSD
-    queue_out = {}
-    # Set some important variables (output-name, number of time steps, number of ensemble members)
-    queue_out['fnme'] = bc_out_lns
-    # queue_out['nts'] = len(coords['time'])
-    # queue_out['nens'] = len(coords['ens'])
-
-
-    # Idea:
-    # - inizialize queue_out only once, and filling this dictionary with the BCSD-corrected data and write output only at the end, when the loop over time is finished
+    
+    # Load the NetCDF to get a handle for the output
+    ds_out = xr.open_dataset(bc_out_lns)
+    
 
 
     # Loop over each variable
-    for v in range(0, 1):
-        print('Variable: ' + vars[v])
-
-        # Set variable-name
-        queue_out['varnme'] = vars[v]
+    for variable in variable_attributes:
+     
 
         ###### Old IO-Module #####
         # load data as dask objects
         # Obs (1981 - 2016 on daily basis)
-        ds_obs = xr.open_mfdataset(list(obs_dict.values())[v], chunks={'time': 13149, 'lat': 1, 'lon': 1})
-        ds_obs = ds_obs[vars[v]]
+        ds_obs = xr.open_mfdataset(list(obs_dict.values())[variable], chunks={'time': 13149, 'lat': 1, 'lon': 1})
+        ds_obs = ds_obs[vars[variable]]
+        
         # Mdl (historical, 1981 - 2016 for one month and 215 days)  215, 36, 25, 1, 1 ;
         # Preprocess historical mdl-data, create a new time coord, which contain year and day at once and not separate
-        ds_mdl = modules.preprocess_mdl_hist(list(mdl_dict.values())[v]) # chunks={'time': 215, 'year': 36, 'ens': 25, 'lat': 1, 'lon': 1})
-        ##### Missing Chunking!!!!
-        ds_mdl = ds_mdl[vars[v]]
+        ds_mdl = modules.preprocess_mdl_hist(list(mdl_dict.values())[variable], month) # chunks={'time': 215, 'year': 36, 'ens': 25, 'lat': 1, 'lon': 1})
+        ds_mdl = ds_mdl[vars[variable]]
+        
         # Pred (current year for one month and 215 days)
-        ds_pred = xr.open_mfdataset(list(pred_dict.values())[v], chunks={'time': 215, 'ens': 51, 'lat': 1, 'lon': 1})
-        ds_pred = ds_pred[vars[v]]
+        ds_pred = xr.open_mfdataset(list(pred_dict.values())[variable], chunks={'time': 215, 'ens': 51, 'lat': 1, 'lon': 1})
+        ds_pred = ds_pred[vars[variable]]
+        
         # Change data type of latidude and longitude, otherwise apply_u_func does not work
         ds_pred = ds_pred.assign_coords(lon=ds_pred.lon.values.astype(np.float32), lat=ds_pred.lat.values.astype(np.float32))
 
@@ -85,43 +92,66 @@ if __name__ == '__main__':
         dayofyear_mdl = ds_mdl['time.dayofyear']
 
         # Set important parameter for BCSD
+        
+        
         window_obs = 15
+        
         if vars[v] == "tp":
-            dry_thresh = 0.01
-            precip = True
-            low_extrapol = "delta_additive"
-            up_extrapol = "delta_additive"
-            extremes = "weibull"
-            intermittency = True
+            bc_params = {
+                'dry_thresh': 0.01,
+                'precip': True,
+                'low_extrapol': "delta_additive",
+                'up_extrapol': "delta_additive",
+                'extremes': "weibull",
+                'intermittency': True,
+                'nquants': 2500
+            }
         else:
-            dry_thresh = 0.01
-            precip = False
-            low_extrapol = "delta_additive"
-            up_extrapol = "delta_additive"
-            extremes = "weibull"
-            intermittency = False
-
-
-        # Set Parameter for Dask
-        dask_allow = False
-        dask_jobs = []
-
-        for k in range(len(coords['time'])):
-            print('# Time step ' + str(k + 1) + ' / ' + str(len(coords['time'])))
-            if dask_allow == True:
-                lst_jobs = dask.delayed(slice_and_correct)(dayofyear_obs, dayofyear_mdl, ds_obs, ds_mdl, ds_pred, coords, queue_out, window_obs, dry_thresh, precip, low_extrapol, up_extrapol, extremes, intermittency, k)
-                dask_jobs.append(lst_jobs)
-            else:
-                slice_and_correct(dayofyear_obs, dayofyear_mdl, ds_obs, ds_mdl, ds_pred, coords, queue_out, window_obs, dry_thresh, precip, low_extrapol, up_extrapol, extremes, intermittency, k)
-
-    print()
-
-    # queue_out, dask_jobs = io_module(obs_dict, mdl_dict, pred_dict, month, bc_out_lns, 15, 15)
-
-
-    # cluster = LocalCluster()
-    # client = Client()
-
-    # dask.compute(*dask_jobs)
-
-
+            bc_params = {
+                'dry_thresh': 0.01,
+                'precip': False,
+                'low_extrapol': "delta_additive",
+                'up_extrapol': "delta_additive",
+                'extremes': "weibull",
+                'intermittency': True,
+                'nquants': 2500
+            }
+            
+            
+            
+            
+        for timestep in range(0, len(ds_pred.time)):
+    
+        
+            day = dayofyear_mdl[timestep]
+    
+            day_range = (np.arange(day - window_obs, day + window_obs + 1) + 365) % 365 + 1
+    
+            intersection_day_obs = np.in1d(dayofyear_obs, day_range)
+            intersection_day_mdl = np.in1d(dayofyear_mdl, day_range)
+    
+            ds_obs_sub = ds_obs.loc[dict(time=intersection_day_obs)]
+    
+            ds_mdl_sub = ds_mdl.loc[dict(time=intersection_day_mdl)]
+        
+            ds_mdl_sub = ds_mdl_sub.stack(ens_time=("ens", "time"), create_index=True)
+            ds_mdl_sub = ds_mdl_sub.drop('time')
+    
+    
+            ds_pred_sub = ds_pred.isel(time=timestep)
+    
+    
+            pred_corr_act = xr.apply_ufunc(
+                bc_module, 
+                ds_pred_sub, 
+                ds_obs_sub, 
+                ds_mdl_sub, 
+                kwargs={'bc_params': bc_params},
+                input_core_dims=[["ens"], ["time"], ['ens_time']], 
+                output_core_dims=[["ens"]], 
+                vectorize=True, 
+                dask="parallelized", 
+                output_dtypes=[np.float64]) 
+    
+    
+            ds_out[variable].loc[dict(time=ds_pred.time.values[timestep])] = pred_corr_act[v].transpose('ens', 'lat', 'lon')
