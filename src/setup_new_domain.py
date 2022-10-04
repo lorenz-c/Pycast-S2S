accc7929952799ed9387448643f0b2baa7e2ec9f
Christof Lorenz <christof.lorenz@kit.edu>
2022-10-04 11:51:01 +0000

    Running version of setup_new_domain

--- src/setup_new_domain.py
+++ src/setup_new_domain.py
@@ -1,45 +1,84 @@
+# import packages
 import json
-import xarray as xr
 import dask
-import numpy as np
-from dask.distributed import Client, LocalCluster
-import importlib
+import argparse
 
 import modules
 import setup_domain_func
 
+import logging
 
-# Get some ressourcers
-#client, cluster = modules.getCluster('fat', 1, 40)
+import os
 
-cluster = LocalCluster()
-client  = Client(cluster)
 
-# Do the memory magic...
-client.amm.start() 
+def get_clas():
     
-# Write some info about the cluster
-print(client.dashboard_link)
+    parser = argparse.ArgumentParser(description="Creation of a new domain for BCSD", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("-d", "--domain", action="store", type=str, help="Domain", required=True)
+    
+    return parser.parse_args()
+
+def setup_logger(domain_name):
+    logging.basicConfig(filename=f"logs/{domain_name}_setup_domain.log", encoding='utf-8', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')
+
+if __name__ == "__main__":
+    
+    args = get_clas()
+    
+    setup_logger(args.domain)
+    
+    # Get some ressourcers
+    client, cluster = modules.getCluster('fat', 1, 25)
+    
+    # Do the memory magic...
+    client.amm.start() 
+    
+    # Write some info about the cluster
+    print(client.scheduler_info)
 
 
-# Read the domain configuration from the respective JSON
-with open('conf/domain_config.json', 'r') as j:
-    domain_config = json.loads(j.read())
+    # Read the domain configuration from the respective JSON
+    with open('conf/domain_config.json', 'r') as j:
+        domain_config = json.loads(j.read())
 
-# Read the global configuration from the respective JSON --> Add this as further input parameter
-with open('conf/global_config.json', 'r') as j:
-    global_config = json.loads(j.read())
+    # Read the global configuration from the respective JSON --> Add this as further input parameter
+    with open('conf/global_config.json', 'r') as j:
+        global_config = json.loads(j.read())
 
-# Read the variable configuration from the respective JSON
-with open('conf/variable_config.json', 'r') as j:
-    variable_config = json.loads(j.read())
+    # Read the variable configuration from the respective JSON
+    with open('conf/variable_config.json', 'r') as j:
+        variable_config = json.loads(j.read())
+    
+    
+    domain_config = domain_config[args.domain]
 
-domain_config = domain_config['germany']
+    variable_config = { key:value for key,value in variable_config.items() if key in domain_config['variables']}
 
-variable_config = { key:value for key,value in variable_config.items() if key in domain_config['variables']}
+    dir_dict = setup_domain_func.set_and_make_dirs(domain_config)
 
-dir_dict = setup_domain_func.set_and_make_dirs(domain_config)
+    grd_fle = setup_domain_func.create_grd_file(domain_config, dir_dict)
 
-grd_fle = setup_domain_func.create_grd_file(domain_config, dir_dict)
+    syr_calib = domain_config["syr_calib"]
+    eyr_calib = domain_config["eyr_calib"]
+    
+    for month in [10, 11, 12]:
+    
+        results = []
+    
+        month_str = str(month).zfill(2)
 
-setup_domain_func.prepare_forecasts(domain_config, variable_config, dir_dict)
+        for year in range(1988, eyr_calib + 1):
+        
+            results.append(setup_domain_func.prepare_forecast_dask(domain_config, variable_config, dir_dict, year, month_str))
+    
+    
+    
+        try:
+            dask.compute(results)
+            logging.info(f"Slicing for month {month_str} for all years from {syr_calib} to {eyr_calib} successful")
+            results = []
+        except:
+            logging.warning(f"Something went wrong during slicing for {month_str}")
+
+        results = []
