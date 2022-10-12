7b8da4d9396cdb68cfc1c0a2bd61d8d5678fe2ee
Christof Lorenz <christof.lorenz@kit.edu>
2022-10-12 15:54:10 +0000

    Users can now run the routine either with a scheduler.json-file or with a separate cluster

--- src/run_bcsd.py
+++ src/run_bcsd.py
@@ -1,43 +1,64 @@
 # import packages
 import json
+from unittest.util import strclass
 import xarray as xr
 import numpy as np
 from dask.distributed import Client
+import dask
 import argparse
 
+import logging
+
 import modules
-import bc_module_v2
+
 
 
 def get_clas():
     
-    parser = argparse.ArgumentParser(description="Python-based BCSD",
-                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(description="Python-based BCSD", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("-d", "--domain", action="store", type=str, help="Domain", required=True)
-    parser.add_argument("-y", "--year", action="store", type=int, help="Year of the actual forecast")
-    parser.add_argument("-m", "--month", action="store", type=int, help="Month of the actual forecast")
-    parser.add_argument("-s", "--forecast_structure", action="store", type=str, help="Structure of the line-chunked forecasts (can be 5D or 4D)")
+    parser.add_argument("-y", "--year", action="store", type=int, help="Year of the actual forecast", required=True)
+    parser.add_argument("-m", "--month", action="store", type=int, help="Month of the actual forecast", required=True)
+    parser.add_argument("-s", "--forecast_structure", action="store", type=str, help="Structure of the line-chunked forecasts (can be 5D or 4D)", required=True)
+    parser.add_argument("-f", "--scheduler_file", action="store", type=str, help="If a scheduler-file is provided, the function does not start its own cluster but rather uses a running environment", required=False)
+    parser.add_argument("-n", "--node", action="store", type=str, help="Node for running the code", required=False)
+    parser.add_argument("-p", "--processes", action="store", type=int, help="Node for running the code", required=False)
     
-    
-
     return parser.parse_args()
     
-    
+def setup_logger(domain_name):
+    logging.basicConfig(filename=f"logs/{domain_name}_run_bcsd.log", encoding='utf-8', level=logging.INFO, format='%(asctime)s:%(levelname)s:%(message)s')   
 
 if __name__ == '__main__':
-
-    # Get some ressourcers
-    client, cluster = modules.getCluster('haswell', 1, 30)
-    #client = Client(scheduler_file='/home/lorenz-c/scheduler.json')
-
-    # Do the memory magic...
+    
+    args = get_clas()
+    
+    if args.scheduler_file is not None:
+        client=Client(scheduler_file=args.scheduler_file)
+    elif args.node is not None:
+        if args.processes is not None:
+            client, cluster = modules.getCluster(args.node, 1,  args.processes)
+        else:
+            logging.error('Run BCSD: If node is provided, you must also set number of processes')
+    else:
+        logging.error('Run BCSD: Must either provide a scheduler file or node and number of processes.')
+        
+    # Make sure that all workers have consistent library versions
+    client.get_versions(check=True)
+    
+    # Do the memory magic...  
     client.amm.start() 
     
+    # It seems as if we need to manually make the two modules available to the workers...
+    #client.upload_file('modules.py') 
+    client.upload_file('bc_module_v2.py')
+    
+    import modules
+    from bc_module_v2 import bc_module
+
     # Write some info about the cluster
     print(f"Dask Dashboard available at {client.dashboard_link}")
-
-    args = get_clas()
     
     # Read the domain configuration from the respective JSON
     with open('conf/domain_config.json', 'r') as j:
@@ -82,27 +103,12 @@ if __name__ == '__main__':
     
     # Create an empty NetCDF in which we write the BCSD output
     ds = modules.create_4d_netcdf(bcsd_dict, global_config, domain_config, variable_config, coords)
-
-
-
-    # Load the NetCDF to get a handle for the output
-   # if domain_config["bcsd_forecasts"]['merged_variables'] == True:
-   #     ds_out = ds
     
     # Loop over each variable
     for variable in variable_config:
-
-    #    if domain_config["bcsd_forecasts"]['merged_variables'] == False:
-     #       ds_out = xr.open_dataset(bcsd_dict[variable])
-
-    #    if variable == 'tp':
-    #        precip_flag = True
-    #    else:
-    #        precip_flag = False
      
         ###### Old IO-Module #####
         # load data as dask objects
-        # Obs (1981 - 2016 on daily basis)
         print(f"Opening {ref_hist_dict[variable]}")
         ds_obs = xr.open_dataset(ref_hist_dict[variable])
         ds_obs = xr.open_mfdataset(ref_hist_dict[variable], chunks={'time': len(ds_obs.time), 'lat': 50, 'lon': 50}, parallel=True, engine='netcdf4')
@@ -118,6 +124,9 @@ if __name__ == '__main__':
             ds_mdl = xr.open_mfdataset(mdl_hist_dict[variable])
             ds_mdl = xr.open_mfdataset(mdl_hist_dict[variable], chunks={'time': len(ds_mdl.time), 'ens': len(ds_mdl.ens), 'lat': 5, 'lon': 5}, parallel=True, engine='netcdf4')
             da_mdl = ds_mdl[variable].persist()
+            
+            
+            
         # IMPLEMENT ELSE-Statement for logging
        
         
@@ -140,10 +149,10 @@ if __name__ == '__main__':
             coords = {
                 'time': ('time', coords['time'], {'standard_name': 'time', 'long_name': 'time'}),
                 'ens': ('ens', coords['ens'], {'standard_name': 'realization', 'long_name': 'ensemble_member'}),
-                'lat': ('lat', coords['lat'], {'standard_name': 'latitude', 'long_name': 'latitude', 'units': 'degrees_east'}),
-                'lon': ('lon', coords['lon'], {'standard_name': 'longitude', 'long_name': 'longitude', 'units': 'degrees_north'})
+                'lat': ('lat', coords['lat'], {'standard_name': 'latitude', 'long_name': 'latitude', 'units': 'degrees_north'}),
+                'lon': ('lon', coords['lon'], {'standard_name': 'longitude', 'long_name': 'longitude', 'units': 'degrees_east'})
             }
-        )
+        ).persist()
             
         for timestep in range(0, len(ds_pred.time)):
             
@@ -154,23 +163,18 @@ if __name__ == '__main__':
             day_range = (np.arange(day - domain_config['bc_params']['window'], day + domain_config['bc_params']['window'] + 1) + 365) % 365 + 1
             intersection_day_obs = np.in1d(dayofyear_obs, day_range)
             intersection_day_mdl = np.in1d(dayofyear_mdl, day_range)
-    
+            
             da_obs_sub = da_obs.loc[dict(time=intersection_day_obs)]
-    
-            da_mdl_sub = da_mdl.loc[dict(time=intersection_day_mdl)]
-        
+            with dask.config.set(**{'array.slicing.split_large_chunks': False}): # --> I really don't know why we need to silence the warning here...
+                da_mdl_sub = da_mdl.loc[dict(time=intersection_day_mdl)]
+                
             da_mdl_sub = da_mdl_sub.stack(ens_time=("ens", "time"), create_index=True)
             da_mdl_sub = da_mdl_sub.drop('time')
-    
-    
-            da_pred_sub = da_pred.isel(time=timestep)
-            
-            print(da_obs_sub.lon)
-            print(da_mdl_sub.lon)
-            print(da_pred_sub.lon)
 
-            pred_corr_act = xr.apply_ufunc(
-                bc_module_v2.bc_module, 
+            da_pred_sub = da_pred.isel(time=timestep)
+
+            da_temp[timestep, :, :] = xr.apply_ufunc(
+                bc_module, 
                 da_pred_sub, 
                 da_obs_sub, 
                 da_mdl_sub, 
@@ -181,8 +185,8 @@ if __name__ == '__main__':
                 dask="parallelized", 
                 output_dtypes=[np.float64]) 
         
-            da_temp.loc[dict(time=ds_pred.time.values[timestep])] = pred_corr_act
-        
+            #da_temp.loc[dict(time=ds_pred.time.values[timestep])] = pred_corr_act
+            #da_temp[timestep, :, :] = pred_corr_act
     
         # Change the datatype from "object" to "float64" --> Can we somehow get around this???
         da_temp = da_temp.astype('float64')
@@ -197,6 +201,8 @@ if __name__ == '__main__':
         ds_out_sel.to_netcdf(bcsd_dict[variable], mode='a', format='NETCDF4_CLASSIC', engine='netcdf4', encoding = {variable: encoding[variable]})
 
     client.close()
-    #cluster.close()
+    
+    if args.scheduler_file is not None:
+        cluster.close()
 
 
