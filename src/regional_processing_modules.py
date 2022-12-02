2f541224194202443ac7e2a7cbb479ada6796878
Clemens Borkenhagen <clemens.borkenhagen9@kit.edu>
2022-12-02 10:33:11 +0100

    Some changes to perform calib_frcst on cluster

--- src/regional_processing_modules.py
+++ src/regional_processing_modules.py
@@ -268,9 +268,9 @@ def rechunk_forecasts(domain_config: dict, variable_config: dict, dir_dict: dict
     except:
         logging.error(f"Something went wrong during writing of forecast linechunks")
 
-
+# domain_config: dict, variable_config: dict, dir_dict: dict, year: int, month: int, variable: str
 @dask.delayed
-def calib_forecasts(domain_config, variable_config, dir_dict, file_list, month_str, variable):
+def calib_forecasts(domain_config: dict, variable_config: dict, dir_dict: dict, file_list: list, month_str: str, variable: str):
     # file_list = []
     # syr =  domain_config['syr_calib']
     # eyr =  domain_config['eyr_calib']
