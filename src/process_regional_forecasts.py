4172ecee77684a161e8e0ee15329a936c290174c
Clemens Borkenhagen <clemens.borkenhagen9@kit.edu>
2022-12-02 09:49:24 +0100

    Some changes to perform calib_frcst on cluster

--- src/process_regional_forecasts.py
+++ src/process_regional_forecasts.py
@@ -169,6 +169,7 @@ if __name__ == "__main__":
                 month_str = str(month).zfill(2)
                 # year = 1981 #dummy
                 results.append(regional_processing_modules.calib_forecasts(domain_config, variable_config, dir_dict, month_str, variable))
+                print(results)
         try:
             dask.compute(results)
             logging.info(f"Calib Forecast: Calibration period successful")
