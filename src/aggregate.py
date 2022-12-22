f401c1a40b2e3456823751c82a8ae83904ec4d54
Clemens Borkenhagen <clemens.borkenhagen9@kit.edu>
2022-12-22 16:32:54 +0100

    Some Changes in order to run aggregation, monthly files, etc.

--- src/aggregate.py
+++ src/aggregate.py
@@ -465,8 +465,8 @@ if __name__ == "__main__":
 
                 # Calculate quantile, tercile and extremes on a monthly basis
                 ds_quintiles = ds.groupby("time.month").quantile(q=[0.2, 0.4, 0.6, 0.8], dim=["time", "ens"])
-                ds_tercile = ds.groupby("time.month").quantile(q=[0.33, 0.66]) # , dim=["ens"])
-                ds_extreme = ds.groupby("time.month").quantile(q=[0.1, 0.9]) # , dim=["ens"])
+                ds_tercile = ds.groupby("time.month").quantile(q=[0.33, 0.66], dim=["time", "ens"]) # , dim=["ens"])
+                ds_extreme = ds.groupby("time.month").quantile(q=[0.1, 0.9], dim=["time", "ens"]) # , dim=["ens"])
                 print(ds_quintiles)
                 # Set Filenames
                 fle_out_quin = f"{domain_config['raw_forecasts']['prefix']}_quintile_{variable}_{syr_calib}_{eyr_calib}_{domain_config['target_resolution']}.nc"
