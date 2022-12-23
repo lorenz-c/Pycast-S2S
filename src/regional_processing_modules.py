0742669107a481cb1a2b326e455fd39ed4fa4735
Clemens Borkenhagen <clemens.borkenhagen9@kit.edu>
2022-12-23 16:14:32 +0100

    Some Changes in order to run aggregation, monthly files, etc.

--- src/regional_processing_modules.py
+++ src/regional_processing_modules.py
@@ -130,7 +130,7 @@ def truncate_forecasts(
         concat_dim="ens",
         combine="nested",
         parallel=True,
-        chunks={"time": -1},
+        chunks={"time": 50},
         preprocess=preprocess,
     )
     # Select Lat/Lon-Box
