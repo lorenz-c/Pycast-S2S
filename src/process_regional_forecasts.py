9817ff22bf2ac4e71fccd6d26f3736efa7c7a957
Clemens Borkenhagen <clemens.borkenhagen9@kit.edu>
2023-01-06 12:17:46 +0100

    Add functionality in order to calculate t2plus and t2minus

--- src/process_regional_forecasts.py
+++ src/process_regional_forecasts.py
@@ -137,7 +137,7 @@ if __name__ == "__main__":
         if key in domain_config["variables"]
     }
 
-
+    print(variable_config)
 
     reg_dir_dict, glob_dir_dict = helper_modules.set_and_make_dirs(domain_config)
 
